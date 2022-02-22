3db5f03f7e48a0f9630e1ae085709fe10bec2e8e
mati <matiexe31@gmail.com>
2022-02-22 15:54:54 -0300

    proyecto base plugin wordpress

---
+++ portal-camara.php
@@ -0,0 +1,16 @@
+/* 
+Plugin Name: Plugin camara de transporte
+Plugin URI: [URL del plugin (si la va a tener, si no puedes poner la tuya)]
+Description: plugin de gestion de cursos de la camara de transporte
+Version: v0.0.1
+Author: Urra Gauna Matias
+Author URI:[URL del autor del plugin]
+License:[El tipo de licencia que le quieras poner. Por ejemplo: GPL]
+*/
+
+
+<?php
+
+
+
+?>
